a0c753ec6ad17ae1fb432ce5e10e21cb46a68973
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 23:32:34 +0800

    build type export header files

---
+++ @export/index.d.ts
@@ -0,0 +1,11 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=NRRD
+
+declare namespace NRRD {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+}
