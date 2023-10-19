d1ef8983b91889e669971e76071db7638e7c112d
guigang xie <xie.guigang@gcmodeller.org>
2023-10-19 14:44:18 +0800

    add dev notes about extra information

--- src/NIFTI/Reader.vb
+++ src/NIFTI/Reader.vb
@@ -8,7 +8,52 @@ Public Class Reader : Implements IDisposable
 
     Public ReadOnly Property headers As Headers
 
+    Public ReadOnly Property ext As String
+        Get
+            If headers Is Nothing Then
+                Return Nothing
+            ElseIf headers.magic = "ni1" Then
+                Return ".hdr"
+            Else
+                Return ".nii"
+            End If
+        End Get
+    End Property
+
     Dim _msg As String
+
+    ''' <summary>
+    ''' Storing extra-information
+    ''' 
+    ''' Extra information can be included in the nifti format in a number 
+    ''' of ways as allowed by the standard. At the end of the header, the 
+    ''' next 4 bytes (i.e., from byte 349 to 352, inclusive) may or may 
+    ''' not be present in a .hdr file. However, these bytes will always 
+    ''' be present in a .nii file. They should be interpreted as a character 
+    ''' array, i.e. char extension[4]. In principle, these 4 bytes should 
+    ''' be all set to zero. If the first, extension[0], is non-zero, this 
+    ''' indicates the presence of extended information beginning at the 
+    ''' byte number 353. Such extended information needs to have size multiple
+    ''' of 16. The first 8 bytes of this extension should be interpreted 
+    ''' as two integers, int esize and int ecode. The field esize indicates
+    ''' the size of the extent, including the first 8 bytes that are the 
+    ''' esize and ecode themselves. The field ecode indicates the format 
+    ''' used for the remaining of the extension. At the time of this 
+    ''' writing, three codes have been defined:
+    ''' 
+    ''' + 0	Unknown. This code should be avoided.
+    ''' + 2	dicom extensions
+    ''' + 4	xml extensions used by the afni software package.
+    ''' 
+    ''' More than one extension can be present in the same file, each one 
+    ''' always starting with the pair esize and ecode, and with its first 
+    ''' byte immediately past the last byte of the previous extension. In 
+    ''' a single .nii file, the float vox_offset must be set properly so 
+    ''' that the imaging data begins only after the end of the last 
+    ''' extension.
+    ''' </summary>
+    Dim _extra As Byte()
+
     Dim disposedValue As Boolean
 
     Sub New(file As BinaryDataReader)
@@ -28,6 +73,10 @@ Public Class Reader : Implements IDisposable
     Public Function LoadHeaders() As Reader
         _headers = New ReaderProvider(file).LoadObject(Of Headers)(offset:=0)
         _msg = _headers.Validate
+
+        If ext = ".nii" Then
+
+        End If
 
         Return Me
     End Function
