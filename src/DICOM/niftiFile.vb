9f68959d2c9fe9554a8b44a6524671136c3c76d0
guigang xie <xie.guigang@gcmodeller.org>
2023-10-19 10:53:23 +0800

    open nifit reader

---
+++ src/DICOM/niftiFile.vb
@@ -0,0 +1,24 @@
+﻿
+Imports System.IO
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports NIFTI
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Components
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("NIFTI")>
+Module niftiFile
+
+    <ExportAPI("open.nifti")>
+    <RApiReturn(GetType(Reader))>
+    Public Function open(<RRawVectorArgument> file As Object, Optional env As Environment = Nothing) As Object
+        Dim buf = SMRUCC.Rsharp.GetFileStream(file, FileAccess.Read, env)
+
+        If buf Like GetType(Message) Then
+            Return buf.TryCast(Of Message)
+        End If
+
+        Return New Reader(buf.TryCast(Of Stream))
+    End Function
+End Module
