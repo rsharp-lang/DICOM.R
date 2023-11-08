db16592b40b8b6334035cc94f95cafb98c18060d
guigang xie <xie.guigang@gcmodeller.org>
2023-11-08 16:59:03 +0800

    config for run reader test success

--- test/readerTest.R
+++ test/readerTest.R
@@ -2,7 +2,8 @@ require(NRRD);
 
 setwd(@dir);
 
-nrrd = NRRD::nrrdRead("..\data\stent.nrrd");
+# nrrd = NRRD::nrrdRead("..\data\stent.nrrd");
+nrrd = NRRD::nrrdRead("..\data\mouseBrain.nrrd");
 # nrrd = NRRD::nrrdRead("F:\Downloads\NRRD.jl-master\test\io\small_time.nrrd");
 header = as.list(NRRD::metadata(nrrd));
 
