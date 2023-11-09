6fcbee6a5800ff3ef924b647dbea7a79f31afa39
guigang xie <xie.guigang@gcmodeller.org>
2023-11-09 11:35:29 +0800

    run package msbuild

---
+++ @export/LASer.d.ts
@@ -0,0 +1,43 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "LASer" from "DICOM";
+//
+// ref=DICOM.LASerFile@DICOM, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+ * 
+*/
+declare namespace LASer {
+   module as {
+      /**
+       * try to cast the raw raster object to las point cloud data
+       * 
+       * 
+        * @param raster -
+        * @param env -
+        * 
+        * + default value Is ``null``.
+      */
+      function las(raster: any, env?: object): object;
+   }
+   /**
+    * Open the file read to the specific las file
+    * 
+    * 
+     * @param lasfile -
+   */
+   function open(lasfile: string): any;
+   /**
+    * load all point cloud model data
+    * 
+    * 
+     * @param las -
+   */
+   function points(las: object): object;
+   module write {
+      /**
+        * @param env default value Is ``null``.
+      */
+      function las(raster: any, file: any, env?: object): any;
+   }
+}
---
+++ man/DICOM/LASer/LASer.md
@@ -0,0 +1,8 @@
+# LASer
+
+
+
++ [open](LASer/open.1) Open the file read to the specific las file
++ [points](LASer/points.1) load all point cloud model data
++ [as.las](LASer/as.las.1) try to cast the raw raster object to las point cloud data
++ [write.las](LASer/write.las.1) 
