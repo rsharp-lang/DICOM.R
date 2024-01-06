accff1b0df89deace3ed58a494b5808417ff388c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-06 20:51:48 +0800

    make improvements of the file writer

--- src/NRRD/FileWriter.vb
+++ src/NRRD/FileWriter.vb
@@ -5,6 +5,7 @@ Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Imaging.BitmapImage
 Imports Microsoft.VisualBasic.Imaging.Drawing2D.HeatMap
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.Text
 
 Public Module FileWriter
 
@@ -25,7 +26,7 @@ Public Module FileWriter
         Call file.Write(str & vbLf, BinaryStringFormat.NoPrefixOrTermination)
     End Sub
 
-    Private Sub WriteAsciiHeaderCommon(file As BinaryDataWriter, dims As Size, len As Integer)
+    Friend Sub WriteAsciiHeaderCommon(file As BinaryDataWriter, dims As Size, len As Integer)
         file.ByteOrder = ByteOrder.LittleEndian
 
         Call file.WriteString(FileReader.MagicBytes)
@@ -43,16 +44,69 @@ Public Module FileWriter
     End Sub
 
     Public Sub WriteFile(file As BinaryDataWriter, dims As Size, rasters As RasterMatrix())
-        Dim intensity As Single()
-
-        Call WriteAsciiHeaderCommon(file, dims, len:=rasters.Length)
-
-        For Each matrix As RasterMatrix In rasters
-            For Each row As Vector In matrix.GetRowScans
-                intensity = row.AsSingle
-                file.Write(intensity)
-            Next
-        Next
+        Dim session As New FileWriterSession(file)
+        Call session.WriteHeader(dims, z:=rasters.Length)
+        Call rasters.DoEach(AddressOf session.WriterRasterLayer)
+        Call file.Flush()
     End Sub
 
 End Module
+
+''' <summary>
+''' File writer session for write large scale raster data model
+''' </summary>
+Public Class FileWriterSession : Implements IDisposable
+
+    ReadOnly file As BinaryDataWriter
+
+    Private disposedValue As Boolean
+
+    Sub New(file As Stream)
+        Me.file = New BinaryDataWriter(file, Encodings.ASCII)
+    End Sub
+
+    Sub New(file As BinaryDataWriter)
+        Me.file = file
+    End Sub
+
+    Public Sub WriteHeader(dims As Size, z As Integer)
+        Call FileWriter.WriteAsciiHeaderCommon(file, dims, len:=z)
+        Call file.Flush()
+    End Sub
+
+    Public Sub WriterRasterLayer(matrix As RasterMatrix)
+        Dim intensity As Single()
+
+        For Each row As Vector In matrix.GetRowScans
+            intensity = row.AsSingle
+            file.Write(intensity)
+        Next
+    End Sub
+
+    Protected Overridable Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                ' TODO: 释放托管状态(托管对象)
+                Call file.Flush()
+                Call file.Dispose()
+            End If
+
+            ' TODO: 释放未托管的资源(未托管的对象)并重写终结器
+            ' TODO: 将大型字段设置为 null
+            disposedValue = True
+        End If
+    End Sub
+
+    ' ' TODO: 仅当“Dispose(disposing As Boolean)”拥有用于释放未托管资源的代码时才替代终结器
+    ' Protected Overrides Sub Finalize()
+    '     ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+    '     Dispose(disposing:=False)
+    '     MyBase.Finalize()
+    ' End Sub
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' 不要更改此代码。请将清理代码放入“Dispose(disposing As Boolean)”方法中
+        Dispose(disposing:=True)
+        GC.SuppressFinalize(Me)
+    End Sub
+End Class
